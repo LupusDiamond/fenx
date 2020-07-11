4ca8663115c5391728d5c8e8064d6a45a1d00870
LupusDiamond <godzplayer@hotmail.com>
2020-07-11 13:35:39 +0300

    index file structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Logo from "./components/Logo/Logo";
+import Cashflow from "./components/Cashflow/Cashflow";
+import CashflowTable from "./components/Cashflow Table/CashflowTable";
 
 class App extends React.Component {
   render() {
-    return <div>"Hello World"</div>;
+    return (
+      <div>
+        <Logo />
+        <Cashflow />
+        <div style={{ display: "flex" }}>
+          <CashflowTable />
+          <CashflowTable />
+        </div>
+      </div>
+    );
   }
 }
 
