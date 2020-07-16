bfc0bd78bf67018e9a37b90311d00a7181ed235e
= <godzplayer@hotmail.com>
2020-07-16 21:54:47 +0300

    included index

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
 const { allowedNodeEnvironmentFlags } = require("process");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 app.listen(port);
