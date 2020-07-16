0be881f4486bb1919731b86426b1497ff58f79b0
= <godzplayer@hotmail.com>
2020-07-16 21:37:09 +0300

    production build

---
+++ build/precache-manifest.c57dd82d1c81e75e43ff0cc1c272bda7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033d909605ef5664701a06cf039505eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6e7dcd0b0618dc8b732",
+    "url": "/static/css/main.7c56e5af.chunk.css"
+  },
+  {
+    "revision": "ca5610a4f3688650af8e",
+    "url": "/static/js/2.8a34f4b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a34f4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e7dcd0b0618dc8b732",
+    "url": "/static/js/main.3086be29.chunk.js"
+  },
+  {
+    "revision": "9e47d95a3eead22c9c1b",
+    "url": "/static/js/runtime-main.aaa4a28c.js"
+  }
+]);
