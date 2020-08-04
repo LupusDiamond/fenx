496791f07a14f0ca8ed25c582c0332b8348e314e
= <godzplayer@hotmail.com>
2020-08-04 17:24:19 +0300

    added VaultCards.js

--- src/components/Vaults.js
+++ src/components/Vaults.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import VaultCard from './VaultCard';
 
 class Vaults extends React.Component {
     render() {
-        return <div>Vaults</div>
+        return (
+        <div className="max-w-6xl w-full mx-auto px-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
+          <VaultCard label="House" amount={200}/>
+          <VaultCard label="College" amount={300}/>
+          <VaultCard label="Private Jet" amount={500}/>
+        </div>
+    </div>
+        )
     }
     
 }
