2973dc8e0e7094690e00e65dacd2bc3be5ce981d
LupusDiamond <godzplayer@hotmail.com>
2020-08-24 19:57:29 +0300

    header

--- src/components/Vaults/Details/Header.js
+++ src/components/Vaults/Details/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import BackSVG from '../../../assets/svgs/Back';
 export default function Header() {
     return (
         <div className="lg:col-span-3 flex justify-between items-baseline">
             <div className="flex items-baseline">
+              <Link to="/vaults">
               <button className="p-1 md:p-2 mr-4 rounded-full border-2 hover:bg-gray-300 border-gray-400 focus:outline-none focus:shadow-outline">
                 <BackSVG />
               </button>
+              </Link>
               <h1 className="text-gray-900 uppercase font-semibold text-2xl md:text-3xl tracking-wide">
                 Vault Details
               </h1>
